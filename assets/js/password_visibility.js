cd3be4248346327cef1895585a57a68482a5aa2c
Noual Ali <noualdev@gmail.com>
2021-06-26 19:50:42 +0100

    Update password_visibility.js

--- assets/js/password_visibility.js
+++ assets/js/password_visibility.js
@@ -2,8 +2,8 @@ function handlePasswordVisibility($input) {
     $button = $input.parentElement.querySelector('.show-password')
     $button.addEventListener('click', () => {
         $icon = $button.querySelector('i')
-        $input.type = $input.type !== 'password' ? 'password' : 'text'
-        $icon.className = $input.type !== 'password' ? 'las la-eye-slash fa-lg' : 'las la-eye fa-lg'
+        $input.type = $input.type == 'text' ? 'password' : 'text'
+        $icon.className = $input.type == 'text' ? 'las la-eye-slash fa-lg' : 'las la-eye fa-lg'
     })
 }
 
