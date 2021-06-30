a4a6fa1be4526d2709b5b1ffe2f02135e67afcaa
Noual Ali <noualdev@gmail.com>
2021-06-30 10:48:54 +0100

    Update FormBuilderServiceProvider.php

--- src/FormBuilderServiceProvider.php
+++ src/FormBuilderServiceProvider.php
@@ -26,8 +26,8 @@ class FormBuilderServiceProvider extends ServiceProvider
         include __DIR__.'/routes.php';
 
         $this->publishes([
-            __DIR__.'/../assets/js' => resource_path('js'),
-            __DIR__.'/../assets/css' => resource_path('css'),
+            __DIR__.'/../assets/js' => public_path('js'),
+            __DIR__.'/../assets/css' => public_path('css'),
         ], 'nldev/FormBuilder');
     }
 }
