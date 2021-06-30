76a435111bd174cf6d165a13c1817799b9922c0d
Noual Ali <noualdev@gmail.com>
2021-06-30 10:40:35 +0100

    Group all js files in one form.js file

--- assets/js/datetimepicker.js
+++
@@ -1,3 +0,0 @@
-const datePickers = bulmaCalendar.attach('[type="date"]');
-const dateTimePickers = bulmaCalendar.attach('[type="datetime"]');
-const timePickers = bulmaCalendar.attach('[type="time"]');
