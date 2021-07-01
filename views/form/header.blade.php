9977c49abeb4bc5cc8454d719856d5d3163d527c
Noual Ali <noualdev@gmail.com>
2021-07-01 18:35:41 +0100

    Update header.blade.php

--- views/form/header.blade.php
+++ views/form/header.blade.php
@@ -2,7 +2,7 @@
 $method = $form->method == 'GET' ? 'GET' : 'POST';
 $media = $form->media ? 'enctype=multipart/form-data' : null;
 @endphp
-<form action="{{ $form->action }}" method="{{ $method }}" {!! $media !!} class="box">
+<form action="{{ route($form->routename, $form->data) }}" method="{{ $method }}" {!! $media !!} class="box">
     @if ($form->title !== null)
         <h1 class="title">{{ $form->title }}</h1>
     @endif
