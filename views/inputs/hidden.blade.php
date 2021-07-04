dea18986caa1bc58538eca951568d79a44772a0f
Noual Ali <noualdev@gmail.com>
2021-07-04 07:56:50 +0100

    Add hidden and readonly inputs

--- views/inputs/hidden.blade.php
+++ views/inputs/hidden.blade.php
@@ -1,5 +1,1 @@
-@php
-    $name = $field->getFieldName();;
-    $value = $field->getValue();
-@endphp
 <input type="hidden" name="{{ $name }}" value="{{ $value }}">
--- views/inputs/readonly.blade.php
+++ views/inputs/readonly.blade.php
@@ -1,17 +1,6 @@
 @php
-$value = $field->getValue();
-$default_message = $field->getDefaultMessage() ?: "Ce champs ne peut pas être modifier";
-$name = $field->getFieldName();
+$default_message = $input->help ?: "Ce champs ne peut pas être modifier";
 @endphp
 
-{{-- Label --}}
-@include('includes.inputs.label')
-
-{{-- Container --}}
-<p class="control{{ $default_message ? ' has-icons-right' : null }}">
-    {{-- Input --}}
-    <input type="text" name="{{ $name }}" id="{{ $name }}" class="input" value="{{ $value }}" readonly>
-
-    {{-- Default message --}}
-    @include('includes.inputs.default_message')
-</p>
+{{-- Input --}}
+<input type="text" name="{{ $name }}" id="{{ $name }}" class="input" value="{{ $value }}" readonly>
