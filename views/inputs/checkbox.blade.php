f932f87e7ddc19ecab0ed5f827c1f0cbaab42b49
Noual Ali <noualdev@gmail.com>
2021-07-16 14:33:25 +0100

    Update checkbox.blade.php

--- views/inputs/checkbox.blade.php
+++ views/inputs/checkbox.blade.php
@@ -3,13 +3,29 @@ $data = $input->data;
 $default_value = $value;
 @endphp
 @if ($data)
-    @foreach ($data as $key => $value)
+    @if (count($data) > 1)
+        @foreach ($data as $key => $value)
+            {{-- Input --}}
+            <input type="checkbox" class="checkbox is-checkradio is-info" name="{{ $name . '[]' }}"
+                id="{{ $value . '-' . $name }}" value="{{ $value }}"
+                {{ (is_array($default_value) ? in_array($value, $default_value) : $default_value == $value) ? 'checked' : null }}>
+            <label for="{{ $value . '-' . $name }}"
+                {{ optional($input->label) ? ' class=has-text-weight-medium' : null }}>
+                {{ $key }}
+            </label>
+        @endforeach
+    @else
+        @php
+            $key = array_key_first($data);
+            $value = $data[$key];
+        @endphp
         {{-- Input --}}
-        <input type="checkbox" class="checkbox is-checkradio is-info" name="{{ $name.'[]' }}" id="{{ $value . '-' . $name }}"
-            value="{{ $value }}" {{ (is_array($default_value) ? in_array($value, $default_value) : $default_value == $value) ? 'checked' : null }}>
+        <input type="checkbox" class="checkbox is-checkradio is-info" name="{{ $name }}"
+            id="{{ $value . '-' . $name }}" value="{{ $value }}"
+            {{ (is_array($default_value) ? in_array($value, $default_value) : $default_value == $value) ? 'checked' : null }}>
         <label for="{{ $value . '-' . $name }}"
             {{ optional($input->label) ? ' class=has-text-weight-medium' : null }}>
             {{ $key }}
         </label>
-    @endforeach
+    @endif
 @endif
